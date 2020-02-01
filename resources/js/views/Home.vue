0193354be1e3de66bcf296d07a89ff59b0ad5141
dagilapon <deykergil28@gmail.com>
2020-02-01 13:50:47 -0400

    Por favor Ramon trabaja con el tab del sublime a 4 perro xD

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -2,92 +2,92 @@
 
 
 
-		<div class="d-flex" id="wrapper">
+    <div class="d-flex" id="wrapper">
 
-    <!-- Sidebar -->
-    <div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading text-center"> CNE </div>
-      <div class="list-group list-group-flush">
-        <a  class="list-group-item list-group-item-action bg-light">solicitud</a>
-        <a  class="list-group-item list-group-item-action bg-light">notificaciones</a>
-        <a  class="list-group-item list-group-item-action bg-light">estadisticas</a>
-        <a  class="list-group-item list-group-item-action bg-light">perfil</a>
-        <a  class="list-group-item list-group-item-action bg-light">usuarios</a>
-        <!-- <a href="#" class="list-group-item list-group-item-action bg-light">Status</a> -->
-      </div>
-    </div>
-    <!-- /#sidebar-wrapper -->
-
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-
-      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="btn btn-primary" id="menu-toggle">Menu</button>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" >Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" @click="logout" >cerrar sesion</a>
-            </li>
-          </ul>
+        <!-- Sidebar -->
+        <div class="bg-light border-right" id="sidebar-wrapper">
+            <div class="sidebar-heading text-center"> CNE </div>
+            <div class="list-group list-group-flush">
+                <a  class="list-group-item list-group-item-action bg-dark">Solicitudes</a>
+                <a  class="list-group-item list-group-item-action bg-light">Notificaciones</a>
+                <a  class="list-group-item list-group-item-action bg-light">Estadisticas</a>
+                <a  class="list-group-item list-group-item-action bg-light">Perfil</a>
+                <a  class="list-group-item list-group-item-action bg-light">Usuarios</a>
+                <!-- <a href="#" class="list-group-item list-group-item-action bg-light">Status</a> -->
+            </div>
         </div>
-      </nav>
+        <!-- /#sidebar-wrapper -->
 
-      <div class="container-fluid">
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                <button class="btn btn-primary" id="menu-toggle">Menu</button>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" >Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" @click="logout" >cerrar sesion</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="container-fluid">
 
 
 
-      </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
     </div>
-    <!-- /#page-content-wrapper -->
-
-  </div>
 
 </template>
 
 <script>
 
-	export default{
+    export default{
 
-		name:'Home',
+        name:'Home',
 
-		mounted () {
+        mounted () {
 
-				$("#menu-toggle").click(function(e) {
-      				e.preventDefault();
-      			$("#wrapper").toggleClass("toggled");
-    				});
+            $("#menu-toggle").click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
 
-		},
+        },
 
-		 methods: {
+        methods: {
 
-		 	logout(){
+            logout(){
 
-				axios.post('logout')
-				.then(response =>{
-				window.location.href = '/';});
+                axios.post('logout')
+                .then(response =>{
+                    window.location.href = '/';});
 
-			},
+            },
 
-		 },
+        },
 
-		 computed:{
+        computed:{
 
 
 
-		},
+        },
 
-	}
+    }
 
 </script>
