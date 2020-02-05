8ced592aa553abc4ebe7f409b62013e4b5387634
dagilapon <deykergil28@gmail.com>
2020-02-04 21:26:54 -0400

    Notificaciones feas

--- resources/js/components/Notificaciones.vue
+++ resources/js/components/Notificaciones.vue
@@ -1,6 +1,18 @@
 <template>
 	<div>
-		<p>notificaciones</p>
+		<div class="alert alert-success" role="alert">
+			<h4 class="alert-heading">Notificaciones si!</h4>
+			<p>Aqui va el texto de la notificacion.</p>
+			<hr>
+			<p>Aqui va el texto de la notificacion.</p>
+		</div>
+
+		<div class="alert alert-danger" role="alert">
+			<h4 class="alert-heading">Notificaciones no!</h4>
+			<p>Aqui va el texto de la notificacion.</p>
+			<hr>
+			<p>Aqui va el texto de la notificacion.</p>
+		</div>
 	</div>
 </template>
 <script>
